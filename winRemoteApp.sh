52a4419d0825787ae218c97835c017a2c4481dfd
jhunt <justin.adam.hunt@gmail.com>
2024-07-16 08:41:03 -0700

    modified:   winRemoteApp.sh Changed home references to $HOME

--- winRemoteApp.sh
+++ winRemoteApp.sh
@@ -8,8 +8,8 @@ lxc init win10 --profile default --empty --vm -c security.secureboot=false -c li
 lxc config device override win10 root size=40GB
 
 # Enable temporary install and drivers media
-echo -n '-drive file=/home/jhunt/ISOs/Tiny10.iso,index=0,media=cdrom,if=ide -drive file=/home/jhunt/win/virtiowin.iso,index=1,media=cdrom,if=ide' | lxc config set win10 raw.qemu
+echo -n '-drive file=$HOME/ISOs/Tiny10.iso,index=0,media=cdrom,if=ide -drive file=/home/jhunt/win/virtiowin.iso,index=1,media=cdrom,if=ide' | lxc config set win10 raw.qemu
 
 # modify apparmor settings for VM read/write permissions
-lxc config set win10 raw.apparor "/home/** rwk,"
+lxc config set win10 raw.apparmor "/home/** rwk,"
 
