5d7ea7158b72934cfc10cf240c70f83aab36ad3f
jadamhunt <justin.adam.hunt@gmail.com>
2024-07-16 08:36:21 -0700

    Add files via upload

---
+++ winRemoteApp.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Create an empty VM
+
+lxc init win10 --profile default --empty --vm -c security.secureboot=false -c limits.cpu=4 -c limits.memory=8GB
+
+# Grow grow root disk
+lxc config device override win10 root size=40GB
+
+# Enable temporary install and drivers media
+echo -n '-drive file=/home/jhunt/ISOs/Tiny10.iso,index=0,media=cdrom,if=ide -drive file=/home/jhunt/win/virtiowin.iso,index=1,media=cdrom,if=ide' | lxc config set win10 raw.qemu
+
+# modify apparmor settings for VM read/write permissions
+lxc config set win10 raw.apparor "/home/** rwk,"
+
